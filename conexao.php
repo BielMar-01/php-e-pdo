35e848e72ea644b22c7740b5566c65ae61a3c72b
Gabriel Marques <bielmarques922@gmail.com>
2022-12-01 21:14:47 -0300

    PHP e PDO - Modulo 02 - Inserindo com exec

--- conexao.php
+++ conexao.php
@@ -1,7 +1,9 @@
 <?php
 
-$caminhoBanco = __DIR__ . '/banco.sqlite';
+$databasePath = __DIR__ . '/banco.sqlite';
 
-$pdo = new PDO('sqlite:' . $caminhoBanco);
+$pdo = new PDO('sqlite:' . $databasePath);
 
 echo'Conectei';
+
+$pdo->exec('CREATE TABLE students(id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
