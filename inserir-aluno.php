95ad0830c8bf89ebf82a1d5a8bf1dd30b8cedd20
Gabriel Marques <bielmarques922@gmail.com>
2022-12-26 09:16:45 -0300

    PHP e PDO - Modulo 03 - Problema de segurança

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,7 +7,10 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '../banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Gabriel Marques', new \DateTimeImmutable('2001-06-27'));
+$student = new Student(
+    null,
+    'Gabriel Marques',
+    new \DateTimeImmutable('2001-06-27'));
 
 $sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
 
